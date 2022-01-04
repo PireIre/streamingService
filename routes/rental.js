66cc88c66bf77ffbbad505d92d9830c1ec1f3b0d
Irenej <irenej@bambuser.com>
2022-01-04 18:07:58 +0100

    Use fawn to handle transaction

--- routes/rental.js
+++ routes/rental.js
@@ -1,13 +1,17 @@
 //Dependencies
 const express = require("express");
+const Fawn = require("fawn");
 const router = express.Router();
 const { Rental, validate } = require("../modules/rental")
 const { Movie } = require("../modules/movie")
 const { User } = require("../modules/user")
+const mongoose = require("mongoose");
+
+Fawn.init(mongoose);
 
 router.get("/", async (req, res) => {
     const rentals = await Rental
-        .find() 
+        .find()
         .sort("-dateOut") //sort by date out in descending order
 
     res.send(rentals)
@@ -33,26 +37,32 @@ router.post("/", async (req, res) => {
     const movie = await Movie.findById(req.body.movieId);
     if (!movie) return res.status(404).send("Movie Id does not exist")
 
-    if(movie.numberInStock === 0) return res.status(400).send("Movie Not in Stock")
+    if (movie.numberInStock === 0) return res.status(400).send("Movie Not in Stock")
 
-    let rental = new Rental({ 
+    let rental = new Rental({
         customer: {
-            _id: customer.id, 
+            _id: customer._id,
             name: customer.name,
             phone: customer.phone
-        }, 
+        },
         movie: {
-            _id: movie.id, 
-            title: movie.title, 
+            _id: movie._id,
+            title: movie.title,
             dailyRentalRate: movie.dailyRentalRate
         },
     })
-    rental = await rental.save();
 
-    movie.numberInStock--;
-    movie.save();
+    try {
+        new Fawn.Task()
+            .save("rentals", rental)
+            .update("movies", { _id: movie._id }, { $inc: { numberInStock: -1 } })
+            .run();
 
-    res.send(rental);
+        res.send(rental);
+    }
+    catch (ext) {
+        res.status(500).send("Something failed.")
+    }
 });
 
 
