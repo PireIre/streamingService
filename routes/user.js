aa8e1bc9f24c026e00bdc1eddf6f8e5b9e384580
Irenej <irenej@bambuser.com>
2022-01-05 11:59:48 +0100

    Hash users passwords

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const _ = require("lodash");
 const router = express.Router();
 const { User, validate } = require("../modules/user")
+const bcrypt = require("bcrypt");
 
 router.get("/", async (req, res)=> {
   const users = await User.find();
@@ -26,6 +27,9 @@ router.post("/", async (req, res)=> {
   if(user) return res.status(400).send("User already exists in the database")
 
   user = new User (_.pick(req.body, ["name", "email", "password"]))
+
+  const salt = await bcrypt.genSalt(10)
+  user.password = await bcrypt.hash(user.password, salt)
 
   await user.save(); 
     
